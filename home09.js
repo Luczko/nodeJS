0826d0a78c07e131ae06400e0f3524817a7cf3f1
Łukasz Wlasiukiewicz <51118963+Luczko@users.noreply.github.com>
2019-11-22 20:51:47 +0100

    Update home09.js

--- home09.js
+++ home09.js
@@ -3,12 +3,12 @@
 const os = require("os");
 const user = os.userInfo();
 
-// setTimeout(() => {
-//   console.log("hello", user.username);
-// }, 5000);
+setTimeout(function() {
+  console.log("hello", user.username);
+}, 5000);
 
-function hello() {
-  console.log("Hello", user.username);
-}
+// function hello() {
+//   console.log("Hello", user.username);
+// }
 
-setTimeout(hello, 5000);
+// setTimeout(hello, 5000);
