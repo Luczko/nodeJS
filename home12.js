b843159097a7f6f8d49c77f9e1db120e7de5ee94
Łukasz Wlasiukiewicz <51118963+Luczko@users.noreply.github.com>
2019-11-24 12:51:44 +0100

    Update home12.js

--- home12.js
+++ home12.js
@@ -1,8 +1,9 @@
 const request = require("request");
 const argv = require("yargs").argv;
 const city = argv.city;
+const town = encodeURI(city);
 
-const newAdress = `https://api.openweathermap.org/data/2.5/weather?APPID=0ed761300a2725ca778c07831ae64d6e&q=${city}`;
+const newAdress = `https://api.openweathermap.org/data/2.5/weather?APPID=0ed761300a2725ca778c07831ae64d6e&q=${town}`;
 
 request(newAdress, function(error, response, body) {
   const data = JSON.parse(body);
